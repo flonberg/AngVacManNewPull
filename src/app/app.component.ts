2da66cfb4433b4e6a32e3bb0a1ab1d2c89a8c0e8
Franklin Lonberg <flonberg@gmail.com>
2021-05-10 13:11:10 -0400

    pick MD and get Data working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MDModality';
+  data: any;
+  planData: any;
+  constructor(private http: HttpClient ) {
+    this.getPlans().subscribe(res =>{
+      this.setData(res);
+    })
+   }
+   /**
+    *  get the plans for a single MD 
+    */
+ getPlans(){
+   var rnd = Math.random();
+  var url = 'https://whiteboard.partners.org/esb/FLwbe/proxy.php?MDKey=test&rnd='+rnd;
+  return this .http.get(url)
+  }
+ setData(res ) {
+   this.data = res;
+   console.log(this.data)
+ }
+ getPlanData(n){
+   console.log("n is %o  key is %o", n, this.data[n]);
+   var url = 'https://whiteboard.partners.org/esb/FLwbe/proxy.php?MDKey=' + n;
+   this .http.get(url).subscribe(res =>{
+     this. setPlanData(res)
+   })
+ }
+ setPlanData(res){
+   this.planData = res;
+   console.log("this pllanData is %o", this.planData)
+ }
+
 }
